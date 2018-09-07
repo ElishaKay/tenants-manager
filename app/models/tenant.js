46c18a488de292185a120da98ac15fdb1a1be9e5
elishakay <kramer1346@gmail.com>
2018-09-07 14:10:47 +0300

    css fix

--- app/models/tenant.js
+++ app/models/tenant.js
@@ -8,6 +8,10 @@ const TenantSchema   = new Schema({
 	id: { type: String, default: uuid },
     name: String,
     email: String,
+    phoneNumber: Number,
+    address: String,
+    debt: 0
+
 });
 
 const Tenant = mongoose.model('Tenant', TenantSchema);
