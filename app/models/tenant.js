65bd90ec7e237911d4d388a6631488570048ccd5
elishakay <kramer1346@gmail.com>
2018-09-07 14:06:37 +0300

    creud works

--- app/models/tenant.js
+++ app/models/tenant.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const uuid         = require('uuid');
 const mongoose     = require('mongoose');
 const Schema       = mongoose.Schema;
 
 const TenantSchema   = new Schema({
+	id: { type: String, default: uuid },
     name: String,
     email: String,
 });
